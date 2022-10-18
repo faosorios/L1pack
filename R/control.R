473b8d9786f9e753fde1ef59cbd01ccd881e34e1
Felipe Osorio <faosorios@gmail.com>
2022-10-18 20:55:20 -0300

    Delete control.R

--- R/control.R
+++
@@ -1,6 +0,0 @@
-## ID: control.R, last updated 2020-10-09, F.Osorio and T.Wolodzko
-
-l1pack.control <- function(maxIter = 2000, tolerance = 1e-9)
-{
-  list(maxIter = maxIter, tolerance = tolerance)
-}
